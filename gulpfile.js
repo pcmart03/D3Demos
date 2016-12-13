d32ed8638cd87553e7575a0917b0d7bcc823d3bc
Patrick Martin <patrick@DESKTOP-SF1BVR0.home>
2016-12-12 23:47:44 -0500

    configures gulp

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const less = require('gulp-less');
+const LessAutoprefix = require('less-plugin-autoprefix');
+const autoprefix = new LessAutoprefix({browsers: ['last 2 versions']});
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require("gulp-uglify-cli");
+const path = require('path');
+
+
+
+gulp.task('less', () => {
+    return gulp.src(path.join(__dirname, 'less', 'style.less'))
+        .pipe(less({
+            paths: [path.join(__dirname, "less", "includes")],
+            plugins: [autoprefix],
+            compress: true
+            }))
+        .pipe(gulp.dest('./css'))
+    });
+
+gulp.task('babel', () => {
+    return gulp.src([path.join(__dirname, 'src','app.js')])
+        .pipe(concat('app.js'))
+        .pipe(babel({
+            presets: ['es2015']
+            }))
+        .pipe(gulp.dest('./js'))
+    });
+
+gulp.task('build', ['less', 'babel']);
